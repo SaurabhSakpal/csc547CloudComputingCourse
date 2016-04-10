685101f6edf7f27712e1d8c25a316d352d844119
SaurabhSakpal <sfstoe@gmail.com>
2016-04-09 23:04:19 -0400

    Directory Link Fixing

--- run-server.sh
+++ run-server.sh
@@ -2,7 +2,9 @@
 
 port=`sh ./getPortNumber.sh`
 echo "$port"
-containerID=`docker run -d -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=sample -e MYSQL_ROOT_PASSWORD=sa123 -p $port:3306 -v /data/mysql:/var/lib/mysql mysql`
+cd /data/mysql/
+sudo mkdir $port
+containerID=`docker run -d -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=sample -e MYSQL_ROOT_PASSWORD=sa123 -p $port:3306 -v /data/mysql/$port:/var/lib/mysql mysql`
 ip=`sh ./getIPAddress.sh $containerID`
 echo "$containerID : $port : $ip">> container_file
 echo "$containerID"
